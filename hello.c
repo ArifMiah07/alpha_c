16cef90016ffa8a9f179f6325bc7a555d6c4cf63
ArifMiah07 <arifmiah.me322@gmail.com>
2025-11-28 11:23:23 +0600

    update circle bin

--- hello.c
+++ hello.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 
 
+
+#define PI 3.1416
+
+float area_of_circle( float radius );
+
 int main()
 {
   printf("Sata Andagi! \n");
+  float area = area_of_circle(21);
+  printf("area of circle : %f \n", area);
   return 0;
+}
+
+float area_of_circle( float radius ){
+  return (PI * radius * radius);
 }
